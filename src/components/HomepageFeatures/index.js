c725b0f8b30de41cf3ac0c61d2fb05a323fb9c7c
Dingan(Will) Wang <anonyknight@hotmail.com>
2023-08-17 00:08:08 -0700

    Polished home page.

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
 
 const FeatureList = [
   {
-    title: 'About Me',
-    img: require('@site/static/img/DinganWang.jpg').default,
-    description: (
-      <>
-        Grinding...
-      </>
-    ),
+    title: "Hello World",
+    image: "/img/DinganWang.jpg",
+    description: <>Welcome to my Internet home.</>,
+  },
+  {
+    title: "Notes",
+    image: "/img/book.svg",
+    description: <>Structured notes for specific topics</>,
+  },
+  {
+    title: "Blogs",
+    image: "/img/blog.svg",
+    description: <>Random topics from daily study, work and life.</>,
   },
 ];
 
-function Feature({img, title, description}) {
+function Feature({ image, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
-        <img src={img} className={styles.featureSvg} role="img" />
+        <img src={image} className={styles.featureSvg} />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
