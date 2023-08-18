ce4c53d962b5f5e5ff0fb57feb798cfb212829ba
Dingan(Will) Wang <anonyknight@hotmail.com>
2023-08-17 23:20:41 -0700

    Modify edit page.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/anonyknight" + baseUrl + "/edit/master/docs/",
+            "https://github.com/anonyknight" + baseUrl + "/edit/master/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/anonyknight/" + baseUrl + "/edit/master/blog/",
+            "https://github.com/anonyknight/" + baseUrl + "/edit/master/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
